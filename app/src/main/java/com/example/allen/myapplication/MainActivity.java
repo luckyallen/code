efa35123441c5b7400a2e8a29bb84890de2eb95f
allen <zhangshuaiuu321@gmail.com>
2018-09-07 16:11:13 +0800

    test

---
+++ app/src/main/java/com/example/allen/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.allen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
